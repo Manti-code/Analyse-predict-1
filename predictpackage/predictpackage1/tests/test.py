45963901ddd79920d995bef2a6a1f072c409fb93
jayramesh01 <61042860+jayramesh01@users.noreply.github.com>
2020-02-19 15:52:23 +0200

    new package stuff

--- predictpackage/predictpackage1/tests/test.py
+++ predictpackage/predictpackage1/tests/test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from predictpackage import predictpackage1
+from predictpackage import predictpackage
 
 ebp_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/electrification_by_province.csv'
 ebp_df = pd.read_csv(ebp_url)
@@ -15,7 +15,7 @@ gauteng = ebp_df['Gauteng'].astype(float).to_list()
 dates = twitter_df['Date'].to_list()
 
 def dictionary_of_metrics(items):
-    assert predictpackage1.fn1_dictionary_of_metrics(gauteng)=={'mean': 26244.42,
+    assert functions.fn1_dictionary_of_metrics(gauteng)=={'mean': 26244.42,
                                                           'median': 24403.5,
                                                           'var': 108160153.17,
                                                           'std': 10400.01,
@@ -23,7 +23,7 @@ def dictionary_of_metrics(items):
                                                           'max': 39660.0} ,"incorrect"
 
 def five_num_summary(items):
-    assert predictpackage1.fn2_five_num_summary(gauteng)== {'max': 39660.0,
+    assert functions.fn2_five_num_summary(gauteng)== {'max': 39660.0,
                                                       'median': 24403.5,
                                                       'min': 8842.0,
                                                       'q1': 18653.0,
